b22e92061dbe0cf2c029ed55696752b670a9127b
Shubhi Gupta <shubhi.gupta@intimetec.com>
2022-11-02 04:05:30 +0530

    UnitTestAssignment

---
+++ UnitTestProject/ConsecutiveDivisorNumber.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace UnitTestProject
+{
+    public class ConsecutiveDivisorNumber
+    {
+        public static void Main(string[] args)
+        {
+            ConsecutiveDivisorNumber consecutiveDivisorNumberObj = new ConsecutiveDivisorNumber();
+            int totalInputs = consecutiveDivisorNumberObj.AskUserForNumberOfInputs();
+            List<long> totalConsecutiveDivisorList = consecutiveDivisorNumberObj.GetTotalDivisor(totalInputs);
+
+            consecutiveDivisorNumberObj.ShowAllDivisor(totalConsecutiveDivisorList);
+            Console.ReadLine();
+        }
+
+        public int AskUserForNumberOfInputs()
+        {
+            int totalInputs = Convert.ToInt32(Console.ReadLine());
+            return totalInputs;
+        }
+
+        public long AskUserForInputs()
+        {
+            long input = Convert.ToInt64(Console.ReadLine());
+            return input;
+        }
+
+        public List<long> GetTotalDivisor(int totalInputs)
+        {
+            List<long> totalConsecutiveDivisorList = new List<long>();
+
+            for (int readInputs = 0; readInputs < totalInputs; readInputs++)
+            {
+                long input = AskUserForInputs();
+                long numberOfDivisor = ConsecutiveDivisor(input);
+                totalConsecutiveDivisorList.Add(numberOfDivisor);
+            }
+
+            return totalConsecutiveDivisorList;
+        }
+
+        public void ShowAllDivisor(List<long> totalConsecutiveDivisorList)
+        {
+            foreach (var showResult in totalConsecutiveDivisorList)
+            {
+                Console.WriteLine(showResult);
+            }
+        }
+
+        public int ConsecutiveDivisor(long inputNumber)
+        {
+            int compareDivisor = 0;
+
+            for (long indexCount = 1; indexCount < inputNumber; indexCount++)
+            {
+                List<long> firstDivisor = GetNumberOfDivisor(indexCount);
+                List<long> secondDivisor = GetNumberOfDivisor(indexCount + 1);
+
+                if (firstDivisor.Count == secondDivisor.Count)
+                    compareDivisor++;
+            }
+            return compareDivisor;
+        }
+
+        public List<long> GetNumberOfDivisor(long divisorLimit)
+        {
+            List<long> totalDivisor = new List<long>();
+            for (long divident = 1; divident < divisorLimit + 1; divident++)
+            {
+                if (divisorLimit % divident == 0)
+                {
+                    totalDivisor.Add(divident);
+                }
+            }
+            return totalDivisor;
+        }
+    }
+}
---
+++ UnitTestProjectTests/ConsecutiveDivisorNumberTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestProject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace UnitTestProject.Tests
+{
+    [TestClass()]
+    public class ConsecutiveDivisorNumberTests
+    {
+        ConsecutiveDivisorNumber divisorNumber = new ConsecutiveDivisorNumber();
+
+        [TestMethod()]
+        public void GetNumberOfDivisorTest_ExpectedOutput()
+        {
+            long divisorLimit = 2;
+            var actual = divisorNumber.GetNumberOfDivisor(divisorLimit);
+            var expected = new List<long> { 1, 2 };
+
+            bool equalValues = Enumerable.SequenceEqual(expected, actual);
+            Assert.IsTrue(equalValues);
+        }
+
+        [TestMethod()]
+        public void ConsecutiveDivisorTest_ExpectedOutput()
+        {
+            long divisorLimit = 3;
+            var actual = divisorNumber.ConsecutiveDivisor(divisorLimit);
+            int expected = 1;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
